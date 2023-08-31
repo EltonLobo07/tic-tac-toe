b20d9358d51e8ab8da1e034907d3f029e1031668
Elton Lobo <eltonsw00@gmail.com>
2023-08-31 10:50:03 +0530

    Add completed modal content views

--- src/components/GameContent.tsx
+++ src/components/GameContent.tsx
@@ -1,4 +1,4 @@
-import { assertNever } from "../helpers/general";
+import { assertNever, joinClasses } from "../helpers/general";
 import { GameType, Mark } from "../type-helpers/app";
 import { Stats } from "../type-helpers/game-content";
 import { Dialog, DialogProps } from "./Dialog";
@@ -7,14 +7,29 @@ import { GameContentMid } from "./GameContentMid";
 import { GameContentTop } from "./GameContentTop";
 import { useState } from "react";
 import { YesNoBtns, YesNoBtnsProps } from "./YesNoBtns";
+import { X } from "./X";
+import { Zero } from "./Zero";
 
 function getPlayerWonKey(wonMark: Mark, playerOneMark: Mark): keyof Stats {
     return wonMark === playerOneMark ? "playerOneWins" : "playerTwoWins";
 }
 
+function getWinLoseMsg(isSoloGame: boolean, didPlayerOneWin: boolean): string {
+    if (isSoloGame) {
+        return didPlayerOneWin ? "you won!" : "oh no, you lost ...";
+    }
+    return `player ${didPlayerOneWin ? "1" : "2"} wins!`;
+}
+
+function didXWin(winnerStr: WinnerStr): boolean {
+    return winnerStr[winnerStr.length - 1].toLowerCase() === "x";
+}
+
+type WinnerStr = `winner-${Mark}`;
+
 type Modal = 
     | "restart"
-    | "clear-winner"
+    | WinnerStr
     | "tie"
     | "none";
 
@@ -63,19 +78,18 @@ export function GameContent(props: Props) {
                 return setCurrentTurnMark(currentTurnMark === "X" ? "0" : "X");
             }
             case "X": {
-                console.log("X is the winner");
                 const playerWonKey = getPlayerWonKey("X", props.playerOneMark);
                 setStats({...stats, [playerWonKey]: stats[playerWonKey] + 1});
+                setOpenModalType("winner-X");
                 return;
             } 
             case "0": {
-                console.log("0 is the winner");
                 const playerWonKey = getPlayerWonKey("0", props.playerOneMark);
                 setStats({...stats, [playerWonKey]: stats[playerWonKey] + 1}); 
+                setOpenModalType("winner-0");
                 return;
             }
             case "draw": {
-                console.log("draw");
                 setStats({...stats, ties: stats.ties + 1});
                 setOpenModalType("tie");
                 return;
@@ -93,44 +107,73 @@ export function GameContent(props: Props) {
         open: openModalType !== "none",
         onClose: closeModal
     };
+    let commonYesNoBtnProps: Pick<YesNoBtnsProps, "yesBtnProps" | "noBtnProps"> = {
+        yesBtnProps: {
+            content: "next round",
+            onClick: handleNextRound
+        },
+        noBtnProps: {
+            content: "quit",
+            onClick: props.onQuit
+        }
+    };
     switch (openModalType) {
         case "none": {
             modal = null;
             break;
         }
-        case "clear-winner": {
+        case "winner-X": 
+        case "winner-0": {
+            const isWinnerX = didXWin(openModalType);
+            const Icon = isWinnerX ? X : Zero;
             modal = (
                 <Dialog
                     {...commonModalProps}
-        
+                    className = "flex flex-col items-center gap-y-4 uppercase py-[40px] tabAndUp:py-[45px]"   
                 >
-                    <div>
-                        Todo
-                    </div>
+                    <span
+                        className = "font-heading-xs text-silver"
+                    >
+                        {getWinLoseMsg(props.gameType === "solo", isWinnerX ? props.playerOneMark === "X" : props.playerOneMark === "0")}
+                    </span>
+                    <Dialog.Title
+                        className = {joinClasses(
+                            "font-heading-m",
+                            "flex gap-x-2 tabAndUp:gap-x-6 items-center",
+                            isWinnerX ? "text-blue-more-green" : "text-dark-yellow"
+                        )}
+                    >
+                        <Icon 
+                            className = "w-[1.875rem] h-[1.875rem] tabAndUp:w-16 tabAndUp:h-16"
+                        />
+                        <span
+                            className = "font-heading-m tabAndUp:font-heading-l"
+                        >
+                            takes the round
+                        </span>
+                    </Dialog.Title>
+                    <YesNoBtns 
+                        {...commonYesNoBtnProps}
+                        className = "mt-2"
+                    />
                 </Dialog>
             );
             break;
         }
         case "restart":
         case "tie": {
-            let titleText = "restart game?";
-            let yesBtnProps: YesNoBtnsProps["yesBtnProps"] = {
-                content: "yes, restart",
-                onClick: handleRestart
-            };
-            let noBtnProps: YesNoBtnsProps["noBtnProps"] = {
-                content: "no, cancel",
-                onClick: closeModal
-            };
-            if (openModalType === "tie") {
-                titleText = "round tied";
-                yesBtnProps = {
-                    content: "next round",
-                    onClick: handleNextRound
-                };
-                noBtnProps = {
-                    content: "quit",
-                    onClick: props.onQuit
+            let titleText = "round tied";
+            if (openModalType === "restart") {
+                titleText = "restart game?";
+                commonYesNoBtnProps = {
+                    yesBtnProps: {
+                        content: "yes, restart",
+                        onClick: handleRestart
+                    },
+                    noBtnProps: {
+                        content: "no, cancel",
+                        onClick: () => setOpenModalType("none")
+                    }
                 };
             }
             modal = (
@@ -144,8 +187,7 @@ export function GameContent(props: Props) {
                         {titleText}
                     </Dialog.Title>
                     <YesNoBtns 
-                        yesBtnProps = {yesBtnProps}
-                        noBtnProps = {noBtnProps}
+                        {...commonYesNoBtnProps}
                     />
                 </Dialog>
             );
